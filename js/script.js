7b2c7b6e05aba504eab0057df0f6f1407b6f4963
Gonzalo Negro <gonzalonnegro@gmail.com>
2023-01-01 19:18:53 -0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,135 @@
+const monedaEl_one = document.getElementById('moneda-uno');
+const monedaEl_two = document.getElementById('moneda-dos');
+const cantidadEl_one = document.getElementById('cantidad-uno');
+const cantidadEl_two = document.getElementById('cantidad-dos');
+const cambioEl = document.getElementById('cambio');
+const tazaEl = document.getElementById('taza');
+
+
+
+function calculate(){
+    const moneda_one = monedaEl_one.value;
+    const moneda_two = monedaEl_two.value;
+
+   fetch(`https://api.exchangerate-api.com/v4/latest/${moneda_one}`)
+   .then(res => res.json() )
+   .then(data => {
+       const taza = data.rates[moneda_two];
+       
+       cambioEl.innerText = `1 ${moneda_one} = ${taza} ${moneda_two}`;
+
+       cantidadEl_two.value = (cantidadEl_one.value * taza).toFixed(2);
+
+    } );
+    
+}
+
+
+monedaEl_one.addEventListener('change', calculate);
+cantidadEl_one.addEventListener('input', calculate);
+monedaEl_two.addEventListener('change', calculate);
+cantidadEl_two.addEventListener('input', calculate);
+
+taza.addEventListener('click', () =>{
+    const temp = monedaEl_one.value;
+    monedaEl_one.value = monedaEl_two.value;
+    monedaEl_two.value = temp;
+    calculate();
+} );
+
+
+calculate();
+
+/* ----------------- */
+const form = document.querySelector("#form-search");
+const moneda = document.querySelector("#moneda");
+const criptomoneda = document.querySelector("#criptomonedas");
+const formContainer = document.querySelector(".form-side");
+const containerAnswer = document.querySelector(".container-answer");
+
+const objBusqueda = {
+    moneda: '',
+    criptomoneda: ''
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    consultarCriptos();
+
+    form.addEventListener('submit', submitForm);
+    moneda.addEventListener('change', getValue);
+    criptomoneda.addEventListener('change', getValue);
+})
+
+function submitForm(e){
+    e.preventDefault();
+    const {moneda, criptomoneda} = objBusqueda;
+    if (moneda === '' || criptomoneda === '') {
+        showError('Seleccione ambas monedas...');
+        return;
+    }
+    consultarAPI(moneda, criptomoneda);
+
+}
+
+function consultarAPI(moneda, criptomoneda){
+    const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
+    fetch(url)
+        .then(resultado => resultado.json())
+        .then(resultadoJson => {
+            mostrarCotizacion(resultadoJson.DISPLAY[criptomoneda][moneda]);
+        })
+        .catch(error => console.log(error));
+}
+
+function mostrarCotizacion(data){
+    clearHTML();
+    const {PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, LASTUPDATE} = data;
+    const answer = document.createElement('div');
+    answer.classList.add('display-info');
+    answer.innerHTML = `
+        <p class="main-price"><u>Precio</u>: <span>${PRICE}</span></p>
+        <p><u>Precio más alto del día</u>: <span>${HIGHDAY}</span></p>
+        <p><u>Precio más bajo del día</u>: <span>${LOWDAY}</span></p>
+        <p><u>Variación últimas 24 horas</u>: <span>${CHANGEPCT24HOUR}%</span></p>
+        <p><u>Última Actualización</u>: <span>${LASTUPDATE}</span></p>
+    `;
+    containerAnswer.appendChild(answer);
+}
+
+function showError(mensage){
+    const error = document.createElement('p');
+    error.classList.add("error");
+    error.textContent = mensage;
+    formContainer.appendChild(error);
+    setTimeout(() => error.remove(), 3000);
+}
+
+function getValue(e){
+    objBusqueda[e.target.name] = e.target.value; 
+}
+
+function consultarCriptos(){
+    const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD';
+    
+    fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(respuestaJson => {
+            selectCriptos(respuestaJson.Data);
+            //console.log(respuestaJson.Data);
+        })
+        .catch(error => console.log(error));
+}
+
+function selectCriptos(criptos){
+    criptos.forEach(cripto => {
+        const {FullName, Name} = cripto.CoinInfo;
+        const option = document.createElement("option");
+        option.value = Name;
+        option.textContent = FullName;
+        criptomoneda.appendChild(option);
+    });
+}
+
+function clearHTML(){
+    containerAnswer.innerHTML = '';
+}
